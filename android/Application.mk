dfb61d5d507ff7d81b261d62f90a3498fd965cbc
zoli <lakizoli@yahoo.com>
2016-03-15 11:29:11 +0100

    GCC 4.9 berakasa az NDK update miatt

--- android/Application.mk
+++ android/Application.mk
@@ -1,5 +1,5 @@
 APP_PLATFORM := android-10
-NDK_TOOLCHAIN_VERSION := 4.8
+NDK_TOOLCHAIN_VERSION := 4.9
 APP_MODULES := c64emu
 APP_STL := c++_shared
 APP_CFLAGS := -Wno-error=format-security -Wno-multichar -Wno-deprecated-register $(VICE_CFLAGS)
